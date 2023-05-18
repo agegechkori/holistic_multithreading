d9b6a31e503207f5733eb7577a2b2617a0d9e2f7
Alex Gegechkori <gecktor@gmail.com>
2023-05-17 23:02:35 -0700

    Added static cast for 0 in std::accumulate call. Formatting changes.

--- code_samples/cpp/thread_sum/sum_ints.cc
+++ code_samples/cpp/thread_sum/sum_ints.cc
@@ -9,7 +9,7 @@
 namespace holistic_multithreading {
 namespace {
 
-void SumValues(int64_t from, int64_t to, int64_t& result) {
+void SumValues(int64_t from, int64_t to, int64_t &result) {
   for (int64_t i = from; i <= to; i++) {
     result += i;
   }
@@ -38,13 +38,15 @@ int64_t MultithreadedSum(int64_t from, int64_t to, int64_t thread_count) {
   for (int64_t i = 0; i < thread_count; i++) {
     int64_t end = (i < thread_count - 1) ? start + batch_size - 1 : to;
     results.push_back(0);
-    threads.push_back(std::thread(SumValues, start, end, std::ref(results.back())));
+    threads.push_back(
+        std::thread(SumValues, start, end, std::ref(results.back())));
     start = end + 1;
   }
-  for (auto& t : threads) {
+  for (auto &t : threads) {
     t.join();
   }
-  return std::accumulate(results.begin(), results.end(), 0);
+  return std::accumulate(results.begin(), results.end(),
+                         static_cast<int64_t>(0));
 }
 
 int64_t SingleThreadSum(int64_t from, int64_t to) {
@@ -53,6 +55,6 @@ int64_t SingleThreadSum(int64_t from, int64_t to) {
     result += i;
   }
   return result;
-}  
+}
 
 } // namespace holistic_multithreading
--- code_samples/cpp/thread_sum/sum_ints_benchmark.cc
+++ code_samples/cpp/thread_sum/sum_ints_benchmark.cc
@@ -2,32 +2,30 @@
 #include <benchmark/benchmark.h>
 #include <thread>
 
-void DoSomething() {
-}
+void DoSomething() {}
 
-
-static void BM_ThreadsSum(benchmark::State& state) {
+static void BM_ThreadsSum(benchmark::State &state) {
   for (auto _ : state)
     holistic_multithreading::ThreadsSum(1, 1000000000);
 }
 // Register the function as a benchmark
 BENCHMARK(BM_ThreadsSum)->UseRealTime();
 
-static void BM_MultithreadedSum(benchmark::State& state) {
+static void BM_MultithreadedSum(benchmark::State &state) {
   for (auto _ : state)
     holistic_multithreading::MultithreadedSum(1, 1000000000, 4);
 }
 // Register the function as a benchmark
 BENCHMARK(BM_MultithreadedSum)->UseRealTime();
 
-static void BM_SingleThreadSum(benchmark::State& state) {
+static void BM_SingleThreadSum(benchmark::State &state) {
   for (auto _ : state)
     holistic_multithreading::SingleThreadSum(1, 1000000000);
 }
 // Register the function as a benchmark
 BENCHMARK(BM_SingleThreadSum);
 
-static void BM_ThreadCreation(benchmark::State& state) {
+static void BM_ThreadCreation(benchmark::State &state) {
   for (auto _ : state) {
     std::thread t(DoSomething);
     t.join();
