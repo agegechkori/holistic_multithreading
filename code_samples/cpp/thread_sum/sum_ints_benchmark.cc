07007c660dc45e49007a13e8223248681fede5a9
Alex Gegechkori <gecktor@gmail.com>
2023-05-22 11:53:01 -0700

    Changed C++ standard to c++17. Added compile flags for clangd. Added ranges to benchmarks.

--- code_samples/cpp/thread_sum/sum_ints_benchmark.cc
+++ code_samples/cpp/thread_sum/sum_ints_benchmark.cc
@@ -1,29 +1,46 @@
 #include "sum_ints.h"
 #include <benchmark/benchmark.h>
+#include <cstdint>
 #include <thread>
+
+namespace holistic_multithreading {
+namespace {
+
+constexpr int64_t kStartRange = 1'000;
+constexpr int64_t kEndRange = 1'000'000'000;
+constexpr int64_t kRangeMultiplier = 1'000;
 
 void DoSomething() {}
 
 static void BM_ThreadsSum(benchmark::State &state) {
   for (auto _ : state)
-    holistic_multithreading::ThreadsSum(1, 1'000'000'000);
+    holistic_multithreading::ThreadsSum(1, state.range(0));
 }
 // Register the function as a benchmark
-BENCHMARK(BM_ThreadsSum)->UseRealTime();
+BENCHMARK(BM_ThreadsSum)
+    ->RangeMultiplier(kRangeMultiplier)
+    ->Range(kStartRange, kEndRange)
+    ->UseRealTime();
 
 static void BM_MultithreadedSum(benchmark::State &state) {
   for (auto _ : state)
-    holistic_multithreading::MultithreadedSum(1, 1'000'000'000, 8);
+    holistic_multithreading::MultithreadedSum(1, state.range(0), 8);
 }
 // Register the function as a benchmark
-BENCHMARK(BM_MultithreadedSum)->UseRealTime();
+BENCHMARK(BM_MultithreadedSum)
+    ->RangeMultiplier(kRangeMultiplier)
+    ->Range(kStartRange, kEndRange)
+    ->UseRealTime();
 
 static void BM_SingleThreadSum(benchmark::State &state) {
   for (auto _ : state)
-    holistic_multithreading::SingleThreadSum(1, 1'000'000'000);
+    holistic_multithreading::SingleThreadSum(1, state.range(0));
 }
 // Register the function as a benchmark
-BENCHMARK(BM_SingleThreadSum);
+BENCHMARK(BM_SingleThreadSum)
+    ->RangeMultiplier(kRangeMultiplier)
+    ->Range(kStartRange, kEndRange)
+    ->UseRealTime();
 
 static void BM_ThreadCreation(benchmark::State &state) {
   for (auto _ : state) {
@@ -32,6 +49,9 @@ static void BM_ThreadCreation(benchmark::State &state) {
   }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_ThreadCreation);
+BENCHMARK(BM_ThreadCreation)->UseRealTime();
+
+} // namespace
+} // namespace holistic_multithreading
 
 BENCHMARK_MAIN();
