235c0dbf2c73e3fc050ffba75eeba71c923a4dd9
Alex Gegechkori <gecktor@gmail.com>
2023-05-22 08:45:12 -0700

    Added optimization flag to the library. Formatting changes..

--- code_samples/cpp/thread_sum/sum_ints_benchmark.cc
+++ code_samples/cpp/thread_sum/sum_ints_benchmark.cc
@@ -6,21 +6,21 @@ void DoSomething() {}
 
 static void BM_ThreadsSum(benchmark::State &state) {
   for (auto _ : state)
-    holistic_multithreading::ThreadsSum(1, 1000000000);
+    holistic_multithreading::ThreadsSum(1, 1'000'000'000);
 }
 // Register the function as a benchmark
 BENCHMARK(BM_ThreadsSum)->UseRealTime();
 
 static void BM_MultithreadedSum(benchmark::State &state) {
   for (auto _ : state)
-    holistic_multithreading::MultithreadedSum(1, 1000000000, 4);
+    holistic_multithreading::MultithreadedSum(1, 1'000'000'000, 8);
 }
 // Register the function as a benchmark
 BENCHMARK(BM_MultithreadedSum)->UseRealTime();
 
 static void BM_SingleThreadSum(benchmark::State &state) {
   for (auto _ : state)
-    holistic_multithreading::SingleThreadSum(1, 1000000000);
+    holistic_multithreading::SingleThreadSum(1, 1'000'000'000);
 }
 // Register the function as a benchmark
 BENCHMARK(BM_SingleThreadSum);
